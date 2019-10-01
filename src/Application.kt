5a289cc8d6b2d14c2fca9f96899c998b89587a3a
Brian Manning <bri@brimanning.com>
2019-10-01 14:57:02 -0400

    Add Koin

--- src/Application.kt
+++ src/Application.kt
@@ -4,22 +4,32 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.koin.dsl.module
+import org.koin.ktor.ext.Koin
+import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
-        }
-    }
-    server.start(wait = true)
+    embeddedServer(Netty, commandLineEnvironment(args)).start(true)
 }
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
+    install(Koin) {
+        modules(module {
+            single { SomeService() }
+        })
+    }
+
+    // Lazy inject HelloService
+    val service: SomeService by inject()
+
+    // Routing section
+    routing {
+        get("/") {
+            call.respondText(service.helloMessage())
+        }
+    }
 }
 
---
+++ src/SomeService.kt
@@ -0,0 +1,7 @@
+package com.brimanning.koinktorexample
+
+import java.util.*
+
+class SomeService {
+    fun helloMessage() = "Hi! The current time in millis is: ${Calendar.getInstance().timeInMillis}"
+}
