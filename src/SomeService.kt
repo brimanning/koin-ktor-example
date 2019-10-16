d3af1e67fa3f9adeec315409c0918cd2184e4470
Brian Manning <bri@brimanning.com>
2019-10-16 11:11:02 -0400

    Update import

--- src/SomeService.kt
+++ src/SomeService.kt
@@ -1,6 +1,6 @@
 package com.brimanning.koinktorexample
 
-import java.util.*
+import java.util.Calendar
 
 class SomeService {
     fun helloMessage() = "Hi from Ktor! The current time in millis is: ${Calendar.getInstance().timeInMillis}"
